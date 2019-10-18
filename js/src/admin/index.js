c134d52c0857f5e7bb0caadefdc17df9f09359cd
David Wheatley <blokmaster.help@gmail.com>
2019-10-18 16:28:54 +0100

    Change from regular FA icons to solid

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -5,7 +5,7 @@ import PermissionGrid from 'flarum/components/PermissionGrid';
 app.initializers.add('fof-nightmode', () => {
   extend(PermissionGrid.prototype, 'startItems', items => {
     items.add('use-dark-mode', {
-      icon: 'far fa-moon',
+      icon: 'fas fa-moon',
       label: app.translator.trans('fof-nightmode.admin.permissions.use'),
       permission: 'user.canUseDarkMode'
     }, 1);
