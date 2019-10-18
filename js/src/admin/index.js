298f4e6cc9abfd8a4b0e7975ab4b8db2033a0857
Ian Morland <ianm@giffgaff.co.uk>
2019-10-18 08:36:43 +0100

    Move permission to startItems

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -3,7 +3,7 @@ import { extend } from 'flarum/extend';
 import PermissionGrid from 'flarum/components/PermissionGrid';
 
 app.initializers.add('fof/nightmode', () => {
-  extend(PermissionGrid.prototype, 'readItems', items => {
+  extend(PermissionGrid.prototype, 'startItems', items => {
     items.add('dark-mode', {
       icon: 'far fa-moon',
       label: app.translator.trans('fof-nightmode.admin.permissions.use'),
