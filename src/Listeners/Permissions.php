c102cb276293517dfeb68d835ea7d9ec713ae0c3
Ian Morland <ianm@giffgaff.co.uk>
2019-10-18 13:18:46 +0100

    Remove unused use statements

--- src/Listeners/Permissions.php
+++ src/Listeners/Permissions.php
@@ -4,11 +4,7 @@ namespace FoF\NightMode\Listeners;
 
 
 use Flarum\Api\Event\Serializing;
-use Flarum\Api\Serializer\BasicDiscussionSerializer;
-use Flarum\Api\Serializer\PostSerializer;
 use Flarum\Api\Serializer\UserSerializer;
-use giffgaff\UserManager\Api\BannedUsers;
-use Illuminate\Cache\Repository;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class Permissions
