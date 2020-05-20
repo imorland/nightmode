6bd8b9e79fa75a3eb20692c4ecf4581ba2cf2319
Ian Morland <ian@morland.me>
2020-05-20 16:54:12 +0000

    Apply fixes from StyleCI

--- extend.php
+++ extend.php
@@ -7,11 +7,11 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('forum'))
-        ->css(__DIR__ . '/resources/less/forum/extension.less')
-        ->js(__DIR__ . '/js/dist/forum.js'),
+        ->css(__DIR__.'/resources/less/forum/extension.less')
+        ->js(__DIR__.'/js/dist/forum.js'),
     (new Extend\Frontend('admin'))
-        ->js(__DIR__ . '/js/dist/admin.js'),
-    new Extend\Locales(__DIR__ . '/resources/locale'),
+        ->js(__DIR__.'/js/dist/admin.js'),
+    new Extend\Locales(__DIR__.'/resources/locale'),
     function (Dispatcher $events) {
         $events->subscribe(Listeners\Preferences::class);
         $events->subscribe(Listeners\Permissions::class);
